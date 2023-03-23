a3f55b1a9359232dbc04147de01f4a1126adf0c4
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-03-23 19:19:48 -0300

    feat: adicionando metodo para listar usuario por email

--- src/modules/User/Infra/MongoDB/IUserRepository.ts
+++ src/modules/User/Infra/MongoDB/IUserRepository.ts
@@ -1,5 +1,8 @@
+import { User } from '../../Entities/User';
+
 interface IUserRepository {
     createUser(name: string, email: string, password: string): void;
+    findUserByEmail(email: string): Promise<User>;
 }
 
 export { IUserRepository };
--- src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
+++ src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
@@ -1,7 +1,15 @@
 import { connect } from '../../../../../shared/mongodb';
+import { User } from '../../../Entities/User';
 import { IUserRepository } from '../IUserRepository';
 
 class UserRepository implements IUserRepository {
+
+    async findUserByEmail(email: string): Promise<User> {
+        const db = await connect();
+        const user = await db.collection('User').findOne({email});
+        return user as unknown as User;
+    }
+
     async createUser(name: string, email: string, password: string): Promise<void> {
         const db = await connect();
         db.collection('User').insertOne({ name, email, password });
