190421983f0c6135cfbb922579a5fc7fa0f1564e
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-03-23 23:30:26 -0300

    feat: adicionando metodo para atualizar refreshToken

--- src/modules/User/Infra/MongoDB/IUserRepository.ts
+++ src/modules/User/Infra/MongoDB/IUserRepository.ts
@@ -3,6 +3,7 @@ import { User } from '../../Entities/User';
 interface IUserRepository {
     createUser(name: string, email: string, password: string): void;
     findUserByEmail(email: string): Promise<User>;
+    updateRefreshToken(userId: string, refreshToken: string, created_at: Date): Promise<void>;
 }
 
 export { IUserRepository };
--- src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
+++ src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
@@ -1,5 +1,6 @@
+import { FindOneAndUpdateOptions, ObjectId, ReturnDocument } from 'mongodb';
 import { connect } from '../../../../../shared/mongodb';
-import { User } from '../../../Entities/User';
+import { RefreshArray, User } from '../../../Entities/User';
 import { IUserRepository } from '../IUserRepository';
 
 class UserRepository implements IUserRepository {
@@ -11,8 +12,18 @@ class UserRepository implements IUserRepository {
     }
 
     async createUser(name: string, email: string, password: string): Promise<void> {
+        const refresh_token: RefreshArray[] = [];
         const db = await connect();
-        db.collection('User').insertOne({ name, email, password });
+        db.collection('User').insertOne({ name, email, password, refresh_token});
+    }
+
+    async updateRefreshToken(id: string, refreshToken: string, created_at: Date): Promise<void> {
+        const db = await connect();
+
+        const filter = {_id: new ObjectId(id)};
+        const update = {  $set: { refresh_token: [refreshToken, created_at] } };
+        const options = { returnOriginal: false } as FindOneAndUpdateOptions;
+        await db.collection('User').findOneAndUpdate(filter, update, options);
     }
 }
 
