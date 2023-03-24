195423ca7c648c8f07fecedf0082263104440f34
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-03-24 10:05:20 -0300

    feat: adicionando rota de task no index principal

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { routerTasks } from './task.routes';
 import { userRoutes } from './user.routes';
 
 const router = Router();
 
 router.use('/user', userRoutes);
+router.use('/task', routerTasks);
 
 export { router };
