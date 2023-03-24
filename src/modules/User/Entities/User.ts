d6b77c1a7142e5675f30e39c32ead99313e8d7a2
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-03-23 23:26:05 -0300

    feat: adicionando atributo para receber refreshToken e createdAt

--- src/modules/User/Entities/User.ts
+++ src/modules/User/Entities/User.ts
@@ -1,14 +1,21 @@
+export type RefreshArray = {
+    refreshToken: string,
+    created_at: Date,
+}
+
 class User {
-    id!: string;
+    _id!: string;
+    refresh_token: RefreshArray[];
     name: string;
     email: string;
     password: string;
 
-    constructor(name:string, email: string, password: string, id: string) {
+    constructor(name:string, email: string, password: string, _id: string, refresh_token: RefreshArray[]) {
         this.name = name;
         this.email =email;
         this.password = password;
-        this.id = id;
+        this._id = _id;
+        this.refresh_token = refresh_token;
     }
 }
 
