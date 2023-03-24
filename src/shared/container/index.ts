4ba9f2ffdb7855d0b6bb163d6e7242f088e85b0c
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-03-24 09:52:21 -0300

    feat: adicionando container para injecao de dependecia

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,8 @@
 import { container } from 'tsyringe';
+import { ITaskRepository } from '../../modules/Task/Infra/MongoDB/ITaskRepository';
+import { TaskRepository } from '../../modules/Task/Infra/MongoDB/Repository/TaskRepository';
 import { IUserRepository } from '../../modules/User/Infra/MongoDB/IUserRepository';
 import { UserRepository } from '../../modules/User/Infra/MongoDB/Repository/UserRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<ITaskRepository>('TaskRepository', TaskRepository);
