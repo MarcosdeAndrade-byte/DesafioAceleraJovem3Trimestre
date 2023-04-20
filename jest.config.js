f76170b7742e3cae8e359b88d76920a930702124
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-04-19 21:14:27 -0300

    build: adicionando dependencias da biblioteca Jest e adicionando arquivo .env no gitingonere

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  preset: 'ts-jest',
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+};
