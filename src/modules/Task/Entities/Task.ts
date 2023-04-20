71738590b241493859ef8c4b9262365206f4ed19
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-04-19 21:59:12 -0300

    feat: adicionando construtor na classe Task

--- src/modules/Task/Entities/Task.ts
+++ src/modules/Task/Entities/Task.ts
@@ -1,12 +1,28 @@
 // Classe Task
 class Task {
-  _id: string;
+  _id!: string;
   userId: string;
   title: string;
   description: string;
   done: boolean;
-  created_at: string;
-  updated_at: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor(
+    userId: string,
+    title: string,
+    description: string,
+    done: boolean,
+    created_at: Date,
+    updated_at: Date
+  ) {
+    this.userId = userId;
+    this.title = title;
+    this.description = description;
+    this.done = done;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
 }
 
 export { Task };
