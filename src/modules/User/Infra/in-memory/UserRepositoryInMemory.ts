520812d29843952f6dcc91e586d85f3dafa75b92
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-04-19 21:24:22 -0300

    feat: adicionando banco de dados em memoria do usuario

---
+++ src/modules/User/Infra/in-memory/UserRepositoryInMemory.ts
@@ -0,0 +1,45 @@
+import { RefreshArray, User } from '../../Entities/User';
+import { IUserRepository } from '../MongoDB/IUserRepository';
+
+class InMemoryUsersRepository implements IUserRepository {
+  private users: User[] = [];
+
+  private id = 1;
+
+  async findUserByEmail(email: string): Promise<User> {
+    const user = this.users.find((u) => u.email === email);
+    return user ?? null;
+  }
+
+  async createUser(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<void> {
+    const refresh_token: RefreshArray[] = [];
+    const user: User = {
+      name,
+      email,
+      password,
+      refresh_token,
+      _id: `${this.id++}`,
+    };
+    this.users.push(user);
+  }
+
+  async updateRefreshToken(
+    id: string,
+    refreshToken: string,
+    created_at: Date
+  ): Promise<void> {
+    const index = this.users.findIndex((u) => u._id === id);
+    if (index === -1) {
+      console.log('DEU RUIM');
+      throw new Error(`User with id ${id} not found`);
+    }
+
+    Object.assign(this.users[index].refresh_token, [refreshToken, created_at]);
+  }
+}
+
+export { InMemoryUsersRepository };
