4a2773896d868c31f6c39ada2cc07d45e47e11dd
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2023-04-21 21:42:25 -0300

    feat: adicionando metodo para deletar usuario por ID

--- src/modules/User/Infra/MongoDB/IUserRepository.ts
+++ src/modules/User/Infra/MongoDB/IUserRepository.ts
@@ -3,13 +3,14 @@ import { User } from '../../Entities/User';
 // Interface ITaskRepository para garantir o baixo acoplamento entre tecnologia utilizada no banco de dados e o nosso código
 interface IUserRepository {
   createUser(name: string, email: string, password: string): void;
-  findUserByEmail(email: string): Promise<User>;
+  findUserByEmail(email: string): Promise<User | null>;
   updateRefreshToken(
     userId: string,
     refreshToken: string,
     created_at: Date
   ): Promise<void>;
   findUserById(userId: string): Promise<User>;
+  deleteById(userId: string): Promise<void>;
 }
 
 export { IUserRepository };
--- src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
+++ src/modules/User/Infra/MongoDB/Repository/UserRepository.ts
@@ -5,16 +5,23 @@ import { IUserRepository } from '../IUserRepository';
 
 // Metodos para manipulação do repositório
 class UserRepository implements IUserRepository {
+  async deleteById(userId: string): Promise<void> {
+    const db = await databaseConnect();
+    db.collection('User').deleteOne({ _id: new ObjectId(userId) });
+  }
+
   async findUserById(userId: string): Promise<User> {
     const db = await databaseConnect();
-    const user = await db.collection('User').findOne({ _id: new ObjectId(userId) });
+    const user = await db
+      .collection('User')
+      .findOne({ _id: new ObjectId(userId) });
     return user as unknown as User;
   }
 
-  async findUserByEmail(email: string): Promise<User> {
+  async findUserByEmail(email: string): Promise<User | null> {
     const db = await databaseConnect();
-    const user = await db.collection('User').findOne({ email });
-    return user as unknown as User;
+    const user = await db.collection<User>('User').findOne({ email });
+    return user;
   }
 
   async createUser(
